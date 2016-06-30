e94f0d8af788af57b8611b0d8d8f8d0b3367d111
MikeyMoore <icarusjedi@gmail.com>
2016-06-30 16:18:18 -0700

    Successful google maps places api

--- app/models/map.rb
+++
@@ -1,9 +0,0 @@
-class Map
-	include HTTParty
-	base_uri ''
-# in progress, this is just a skeleton
-	# def getMap
-	# 	@map = self.class.get("???/#{SECRET_TOKEN}")
-	# end
-
-end
--- public/js/application.js
+++ public/js/application.js
@@ -1,64 +1,73 @@
 // new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
 
-function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
+// function initAutocomplete() {
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 37.7749, lng: -122.4194},
+//           zoom: 13,
+//           mapTypeId: google.maps.MapTypeId.ROADMAP
+//         });
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//         // Create the search box and link it to the UI element.
+//         var input = document.getElementById('pac-input');
+//         var searchBox = new google.maps.places.SearchBox(input);
+//         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+//         // Bias the SearchBox results towards current map's viewport.
+//         map.addListener('bounds_changed', function() {
+//           searchBox.setBounds(map.getBounds());
+//         });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+//         var markers = [];
+//         // Listen for the event fired when the user selects a prediction and retrieve
+//         // more details for that place.
+//         searchBox.addListener('places_changed', function() {
+//           var places = searchBox.getPlaces();
 
-          if (places.length == 0) {
-            return;
-          }
+//           if (places.length == 0) {
+//             return;
+//           }
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+//           // Clear out the old markers.
+//           markers.forEach(function(marker) {
+//             marker.setMap(null);
+//           });
+//           markers = [];
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
+//           // For each place, get the icon, name and location.
+//           var bounds = new google.maps.LatLngBounds();
+//           places.forEach(function(place) {
+//             var icon = {
+//               url: place.icon,
+//               size: new google.maps.Size(71, 71),
+//               origin: new google.maps.Point(0, 0),
+//               anchor: new google.maps.Point(17, 34),
+//               scaledSize: new google.maps.Size(25, 25)
+//             };
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
+//             // Create a marker for each place.
+//             markers.push(new google.maps.Marker({
+//               map: map,
+//               icon: icon,
+//               title: place.name,
+//               position: place.geometry.location
+//             }));
 
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-      }
+//             if (place.geometry.viewport) {
+//               // Only geocodes have viewport.
+//               bounds.union(place.geometry.viewport);
+//             } else {
+//               bounds.extend(place.geometry.location);
+//             }
+//           });
+//           map.fitBounds(bounds);
+//         });
+//       }
+ 
+
+   // var ac = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+   // google.maps.event.addListener(ac, 'place_changed', function(){
+   // 	var place = ac.getPlace();
+   // 	console.log(place.formatted_address);
+   // 	console.log(place.url);
+   // 	console.log(place.geometry.location);
+   // })
